2ea3769be675f8a92b38b58a84703c8659d57a1b
apetrogianni <apetrogianni@uth.gr>
2023-04-05 22:31:12 +0300

    minor

--- utils/feature_extraction.py
+++ utils/feature_extraction.py
@@ -8,6 +8,7 @@ from pyAudioAnalysis import MidTermFeatures as aF
 from pyAudioAnalysis import ShortTermFeatures as sF
 
 def feature_extractor(dirs: str, 
+                      num_of_classes: int,
                       mid_window: float = 1.0, 
                       mid_step: float = 1.0, 
                       short_window: float = 0.05,
@@ -42,5 +43,19 @@ def feature_extractor(dirs: str,
                     " folder is empty or non-existing!")
             return
     
-    return features, class_names, file_names
+    shapes_list = [arr.shape[0] for arr in features]
+
+    feat = np.concatenate(features, axis=0)
+    features_list = feat.tolist()
+
+    print(f"\n There are: {len(features_list)} feature vectors.")
+    
+    # features_list contains all the feature vectors
+    np.save(f'features_{num_of_classes}_classes.npy', features_list)
+    # file_names contains all the corresponding file names
+    np.save(f'wav_names_{num_of_classes}_classes.npy', file_names)
+    # shape_list contains the number of files in each class
+    np.save(f'shapes_{num_of_classes}_classes.npy', shapes_list)
+    
+    return features_list, class_names, file_names
             
