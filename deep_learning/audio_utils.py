b066f5966df19e71339686c8bae944909f744072
Ellie Vakalaki <evakalakiaidl@gmail.com>
2023-04-07 15:30:18 +0300

    [Add] extract_mel_spectrogram in audio_utils

---
+++ deep_learning/audio_utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+import librosa
+
+def extract_mel_spectrogram(
+    signal: np.ndarray, sr: int = 8000, n_fft: int = 1024, hop_length: int = 256, n_mels: int = 256
+) -> np.ndarray:
+
+    S = librosa.feature.melspectrogram(y=signal, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
+    # convert to dB for log-power mel-spectrograms
+    return librosa.power_to_db(S, ref=np.max)
