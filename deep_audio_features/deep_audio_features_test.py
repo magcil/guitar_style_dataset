f3c9b493292fc7424fefb68a065d3941e69b7101
Christos Nikou <chrisnick92@gmail.com>
2023-04-10 16:39:58 +0300

    Main part and utility functions completed

--- deep_audio_features/deep_audio_features_test.py
+++ deep_audio_features/deep_audio_features_test.py
@@ -1,6 +1,12 @@
 import argparse
+import json
+import os
+import shutil
 
-from deep_audio_utils import prepare_dirs, deep_audio_training, validate_on_test
+from sklearn.metrics import accuracy_score, recall_score, precision_score
+
+from deep_audio_utils import prepare_dirs, deep_audio_training, validate_on_test, crawl_directory
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -14,6 +20,54 @@ def parse_args():
 
     return parser.parse_args()
 
+
 if __name__ == '__main__':
     """Train and validate on folds using deep audio features."""
-    pass
+
+    args = parse_args()
+    input_data_path = args.input_data
+    json_file = args.json_file
+    segment_size = args.segment_size
+    output_path = args.output_dir_path
+
+    with open(json_file, 'r') as f:
+        splits = json.load(f)
+    songs = crawl_directory(input_data_path)
+    accs, recalls, precisions, f1_scores = [], [], [], []
+    for fold in splits:
+        train_wavs, test_wavs = [], []
+        # Split to train/test
+        train_set = splits[fold]['train']
+        test_set = splits[fold]['test']
+        for song in songs:
+            if os.path.basename(song) in train_set:
+                train_wavs.append(song)
+            else:
+                test_wavs.append(song)
+        print('Fold:', fold)
+        print('Number in test:', len(test_wavs))
+        print('Number in train:', len(train_wavs))
+
+        print(f'Preparing dirs...')
+        prepare_dirs(train_wavs, test_wavs, output_path, segment_size)
+        print('Training starts...')
+        class_mapping = deep_audio_training(output_path)
+        y_true, y_pred = validate_on_test(output_path, class_mapping)
+
+        acc = 100 * accuracy_score(y_true, y_pred)
+        precision = 100 * precision_score(y_true, y_pred)
+        recall = 100 * recall_score(y_true, y_pred)
+        f1_score = 100 * (precision * recall) / (precision + recall)
+
+        print(
+            f'Results on {fold}\nAccuracy: {acc:.2f}\nRecall: {recall:.2f}'
+            f'\nPrecision: {precision:.2f}\nF1: {f1_score:2f}'
+        )
+        accs.append(acc)
+        precisions.append(precision)
+        recalls.append(recall)
+        f1_scores.append(f1_score)
+
+        # Remove dirs
+        shutil.rmtree(os.path.join(output_path, "train"))
+        shutil.rmtree(os.path.join(output_path, "test"))
--- deep_audio_features/deep_audio_utils.py
+++ deep_audio_features/deep_audio_utils.py
@@ -1,4 +1,12 @@
 import os
+import pickle
+
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+
+from deep_audio_features.bin import basic_training as bt
+from deep_audio_features.bin import basic_test as btest
 
 CLASS_MAPPING = {
     0: 'alternate picking',
@@ -11,6 +19,7 @@ CLASS_MAPPING = {
     7: 'slide',
     8: 'bend'
 }
+
 
 def crawl_directory(directory: str, extension: str = None) -> list:
     """Crawling data directory
@@ -32,6 +41,7 @@ def crawl_directory(directory: str, extension: str = None) -> list:
                 tree.append(os.path.join(subdir, _file))
     return tree
 
+
 def prepare_dirs(train_wavs, test_wavs, output_path, segment_size):
     """Given a train/test split create dirs with segmented wavs on train separated in classes"""
     train_path = os.path.join(output_path, 'train')
@@ -39,16 +49,38 @@ def prepare_dirs(train_wavs, test_wavs, output_path, segment_size):
 
     # Prepare test dirs
     for label, guitar_technique in CLASS_MAPPING.items():
-        
+
         pass
     pass
 
 
-def deep_audio_training():
+def deep_audio_training(output_path):
     """Train on dirs using deep audio features"""
-    pass
+
+    train_path = os.path.join(output_path, 'train')
+    train_dirs = next(os.walk(train_path))[1]
+    bt.train_model(train_dirs, 'technique_classifier.pt')
+
+    return {i: technique for (i, technique) in enumerate(train_dirs)}
 
 
-def validate_on_test():
+def validate_on_test(output_path, class_mapping, model_path='pkl/technique_classifier.pt'):
     """Validate on test using deep audio features"""
-    pass
+    test_path = os.path.join(output_path, 'test')
+    y_true, y_pred = [], []
+    test_songs = crawl_directory(test_path)
+
+    for song in test_songs:
+        preds, posteriors = btest.test_model(model_path, song, layers_dropped=0, test_segmentation=True)
+        probs = np.exp(posteriors) / np.sum(np.exp(posteriors), axis=1).reshape(posteriors.shape[0], 1)
+        p_aggregated = probs.mean(axis=0)
+        preds = np.bincount(preds)
+        res = []
+        for i in range(preds.size):
+            res.append((preds[i], p_aggregated[i], class_mapping[i]))
+        res = sorted(res, key=lambda x: (x[0], x[1]), reverse=True)
+        y_pred.append(res[0][2])
+        song_label = int(os.path.basename(song).split('_')[1])
+        y_true.append(class_mapping[song_label])
+
+    return y_true, y_pred
