7a39597d269aebb4f9a20c1e6e0f98484b1ceb2e
apetrogianni <apetrogianni@uth.gr>
2023-04-10 16:22:37 +0300

    GridSearch

--- train.py
+++ train.py
@@ -10,8 +10,7 @@ from data_load.dataloading import data_preparation_and_train
 
 """
 
-python3 train.py -w '/media/antonia/Seagate/GitHub/guitar_style_dataset/data/alternate picking' '/media/antonia/Seagate/GitHub/guitar_style_dataset/data/legato' '/media/antonia/Seagate/GitHub/guitar_style_dataset/data/tapping' '/media/antonia/Seagate/GitHub/guitar_style_dataset/data/sweep picking' '/media/antonia/Seagate/GitHub/guitar_style_dataset/data/vibrato' '/media/antonia/Seagate/GitHub/guitar_style_dataset/data/hammer on' '/media/antonia/Seagate/GitHub/guitar_style_dataset/data/pull off' '/media/antonia/Seagate/GitHub/guitar_style_dataset/data/slide' '/media/antonia/Seagate/GitHub/guitar_style_dataset/data/bend' -rf folds.json 
-
+python3 train.py -d data/wav -rf data/folds.json
 
 """
 
@@ -22,12 +21,11 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description="Guitar Style Classifcation")
 
     parser.add_argument(
-        "-w", 
-        "--wav_path", 
+        "-d", 
+        "--data_path", 
         required=True, 
-        action='append',
-        nargs='+', 
-        help="The paths to the WAV files.")
+        type=str,
+        help="The paths to the directories containing the WAV files.")
     
     parser.add_argument(
         "-f",
@@ -52,14 +50,19 @@ if __name__ == "__main__":
     warnings.filterwarnings('ignore')
     
     args = parse_arguments()
-    wav_path = args.wav_path
+    data_path = args.data_path
     fold = args.fold
     ready_folds = args.ready_folds
+
+    print("Guitar Style Classes: ", data_path)
     
-    print("Guitar Style Classes: ", wav_path)
+    class_folders = []
     
-    wav_path = [item for sublist in wav_path for item in sublist]
-    data_preparation_and_train(wav_path, fold, ready_folds)
+    for folder in os.listdir(data_path):
+        if os.path.isdir(os.path.join(data_path, folder)):
+            class_folders.append(os.path.join(data_path, folder))
     
-        
+    print("Classes: ", class_folders)
+    data_preparation_and_train(class_folders, fold, ready_folds)
+    
     
