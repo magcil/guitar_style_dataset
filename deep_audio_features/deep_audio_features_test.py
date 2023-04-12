3c9536693766cf527f89e0e250dcc5beafc094a1
Christos Nikou <chrisnick92@gmail.com>
2023-04-12 15:03:15 +0300

    Better format of output txt

--- deep_audio_features/deep_audio_features_test.py
+++ deep_audio_features/deep_audio_features_test.py
@@ -3,6 +3,7 @@ import json
 import os
 import shutil
 import sys
+import pathlib
 
 from sklearn.metrics import accuracy_score, recall_score, precision_score, classification_report, confusion_matrix
 import numpy as np
@@ -40,7 +41,7 @@ if __name__ == '__main__':
     output_path = args.output_dir_path
     
     logs = []
-    aggregated_cm = np.zeros((9, 9), dtype=np.int64)
+    aggregated_cm = np.zeros((9, 9), dtype=int)
 
     with open(json_file, 'r') as f:
         splits = json.load(f)
@@ -64,7 +65,7 @@ if __name__ == '__main__':
         print('Fold:', fold)
         print('Number in test:', len(test_wavs))
         print('Number in train:', len(train_wavs))
-        logs.append(f'{5*"-"}> {fold} <{5*"-"}\n')
+        logs.append(f'\n{20*"-"}> {fold} <{20*"-"}\n')
 
         print(f'Preparing dirs...')
         prepare_dirs(input_data_path, train_wavs, test_wavs, output_path, segment_size)
@@ -81,32 +82,41 @@ if __name__ == '__main__':
         recall = recall_score(y_true, y_pred, average='macro', zero_division=0, labels=labels)
         f1_score = 2 * (precision * recall) / (precision + recall)
 
-        print(
-            f'Results on {fold}\nAccuracy: {100* acc:.2f}\nRecall: {100* recall:.2f}'
-            f'\nPrecision: {100* precision:.2f}\nF1: {100 * f1_score:2f}'
-        )
+        fold_results = f'Results on {fold}\nAccuracy: {100* acc:.2f}\nRecall: {100* recall:.2f}' +\
+        f'\nPrecision: {100* precision:.2f}\nF1: {100 * f1_score:2f}'
+        print(fold_results)
+
         accs.append(acc)
         precisions.append(precision)
         recalls.append(recall)
         f1_scores.append(f1_score)
         class_rep = classification_report(y_true, y_pred, labels=labels)
         logs.append(class_rep)
+        logs.append(fold_results)
+        logs.append(f'\nConfusion Matrix:\n{cm}\n')
 
         # Remove dirs
         shutil.rmtree(os.path.join(output_path, "train"))
         shutil.rmtree(os.path.join(output_path, "test"))
 
-    logs.append('Total Scores\n')
+    logs.append(f'\n{25*"-"}> Total Scores <{25*"-"}\n')
     final_results = f'Mean Accuracy: {100 * sum(accs) / len(accs)}\n' + \
     f'Mean Precision: {100 * sum(precisions) / len(precisions)}\n' + \
     f'Mean Recall: {100 * sum(recalls) / len(recalls)}\n' + \
     f'Mean F1 score: {100 * sum(f1_scores) / len(f1_scores)}\n' + \
-    f'Std F1 score: {100 * np.std(f1_scores)}\n'
+    f'Std F1 score: {100 * np.std(f1_scores)}\n' + \
+    f'Aggregated Confusion Matrix:\n{aggregated_cm}\n'
 
     logs.append(final_results)
     print(final_results)
 
-    with open('deep_audio_results.txt', 'w') as f:
+    if 'guitar' in json_file.lower():
+        out_txt = 'guitar_folds_cnn.txt'
+    elif 'amplifier' in json_file.lower():
+        out_txt = 'amplifier_folds_cnn.txt'
+    else:
+        out_txt = 'exercise_folds_cnn.txt'
+    with open(out_txt, 'w') as f:
         f.writelines(logs)
 
     # Confusion matrix
